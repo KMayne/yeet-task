8d6a5f32efff5118265eb60e2ae0813443579e6e
Kian Mayne <git@kianmayne.co.uk>
2019-11-30 22:28:44 +0000

    Add documentation endpoint

--- src/api-router.js
+++ src/api-router.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    greetings: 'Hi'
+  });
+});
+
 router.get('/tasks', (req, res) => {
   res.json([
     { id: 1, text: 'Item 1', done: false },
